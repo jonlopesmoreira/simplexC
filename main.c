7a7ddd0ab01f5ad819337db1c29b3991bbcd5d13
jonlopesmoreira <jonprojetos@hotmail.com>
2020-07-20 16:50:24 -0300

    fixed launch.json and debug mode

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 #include "tokenizacao.c"
 #include "funcoesAuxiliares.c"
 
+
+int fib(int n)
+{
+    if(n<=2)
+        return 1;
+    else
+        return fib(n-1) + fib(n-2);
+}
 int main()
 {
     int numLinhas, numColunas;
